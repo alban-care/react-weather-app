15287da26d0df3709f0012ffdbd7a86dba4e7146
Alban Caré <alban.care@gmail.com>
2023-06-02 16:02:39 +0200

    install material-ui and its dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import App from "./components/App.tsx";
+import theme from "./theme.ts";
 
-const root= document.getElementById('root')
+const root = document.getElementById("root");
 
 ReactDOM.createRoot(root as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+    <CssBaseline />
+  </React.StrictMode>
+);
